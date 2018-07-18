4f6fe08bd2fb7525e00fdb70cd02fa127c2c2d31
jasonp <jasonp@graazesoft.com>
2018-07-18 13:13:20 -0400

    Extract rudundancy, prepare for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -8,7 +10,7 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-     @article = Article.find(params[:id])
+     
   end
   
      
@@ -26,11 +28,11 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+    
   end
   
   def update
-    @article = Article.find(params[:id])
+    
     
     if @article.update(articles_params)
       flash[:notice] = "Article was update"
@@ -42,13 +44,17 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    
     @article.destroy
     flash[:notice] = "Article was deleted"
     redirect_to articles_path
   end  
   
   private
+  
+  def set_article
+    @article = Article.find(params[:id])
+  end
   
   def articles_params
    params.require(:article).permit(:title, :description)  

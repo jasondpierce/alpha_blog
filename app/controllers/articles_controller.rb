101084315b8a26e9c2cf941e7771ee9937f65640
jasonp <jasonp@graazesoft.com>
2018-07-18 10:22:01 -0400

    create new template, action and router

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,19 @@
-class ArticleController < ActionController::Base
+class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+  
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(articles_params)
+    @article.save
+    redirect_to articles_show(@article)
+  end
+  
+  private
+  
+  def articles_params
+   params.require(:article).permit(:title, :description)  
+  end
+  
 end

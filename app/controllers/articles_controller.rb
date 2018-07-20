97c06b5b8811097be28ed3cfd2bc781cd3eb0f3c
jasonp <jasonp@graazesoft.com>
2018-07-20 10:33:06 -0400

    Update current use to be article creatore

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
   def create
     
     @article = Article.new(articles_params)
-    @article.user = User.first
+    @article.user = current_user
     
     if @article.save
       flash[:success] = "Article was successfully created"
